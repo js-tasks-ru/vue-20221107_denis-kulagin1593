d78857349382b56827ecb918096dac2943f10a47
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-11-11 22:34:36 +0700

    complet

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,27 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const calculatorOperators = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      a: 10,
+      b: 0,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    result() {
+      return calculatorOperators[this.operator](this.a, this.b);
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
