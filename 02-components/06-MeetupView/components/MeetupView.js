d0ca0458939e255ed2432040be602709ac25c55f
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-11-17 20:03:26 +0700

    MeetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,6 +1,10 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupAgenda from './MeetupAgenda';
+import MeetupDescription from './MeetupDescription';
+import MeetupInfo from './MeetupInfo';
+import MeetupCover from './MeetupCover';
 
 export default defineComponent({
   name: 'MeetupView',
@@ -8,6 +12,10 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupCover,
+    MeetupDescription,
+    MeetupAgenda,
+    MeetupInfo,
   },
 
   props: {
@@ -19,22 +27,29 @@ export default defineComponent({
 
   template: `
     <div>
-      <!-- meetup cover-->
+    <MeetupCover :title="meetup['title']" :image="meetup['image']"/>
 
-      <ui-container>
-        <div class="meetup">
-          <div class="meetup__content">
-            <h3>Описание</h3>
-            <!-- meetup description -->
+    <ui-container>
+      <div class="meetup">
+        <div class="meetup__content">
+          <h3>Описание</h3>
+          <MeetupDescription :description="meetup['description']"/>
 
-            <h3>Программа</h3>
-            <!-- meetup agenda -->
-            <ui-alert>Программа пока пуста...</ui-alert>
-          </div>
-          <div class="meetup__aside">
-            <!-- meetup info -->
-          </div>
+          <h3>Программа</h3>
+          <MeetupAgenda v-if="meetup['agenda']" :agenda="meetup['agenda']"/>
+          <ui-alert v-else>Программа пока пуста...</ui-alert>
         </div>
-      </ui-container>
-    </div>`,
+        <div class="meetup__aside">
+          <MeetupInfo :date="meetup['date']" :place="meetup['place']" :organizer="meetup['organizer']"/>
+        </div>
+      </div>
+    </ui-container>
+    </div>
+    <script>
+    import MeetupDescription from "./MeetupDescription";
+
+    export default {
+      components: {MeetupDescription}
+    }
+    </script>`,
 });
