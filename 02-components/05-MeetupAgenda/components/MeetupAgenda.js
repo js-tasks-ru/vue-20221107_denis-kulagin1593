e7ff6999c745c88ef56a83854b0f51025a1dddfe
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-11-17 19:37:13 +0700

    MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,30 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  components: { MeetupAgendaItem },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
+
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
-      </li>
+    <li class="agenda__item">
+      <div>
+        <MeetupAgendaItem :agenda-item="agenda[0]"/>
+      </div>
+      <div>
+        <MeetupAgendaItem :agenda-item="agenda[1]"/>
+      </div>
+      <div>
+        <MeetupAgendaItem :agenda-item="agenda[2]"/>
+      </div>
+    </li>
     </ul>`,
 });
