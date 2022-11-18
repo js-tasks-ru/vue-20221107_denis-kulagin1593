3830f19e1677b0c8d9fcdb463ece27bd5568626f
Denis Kulagin <denis.kulagin1593@mail.ru>
2022-11-18 17:44:37 +0700

    Fix meetup View

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -36,7 +36,7 @@ export default defineComponent({
           <MeetupDescription :description="meetup['description']"/>
 
           <h3>Программа</h3>
-          <MeetupAgenda v-if="meetup['agenda']" :agenda="meetup['agenda']"/>
+          <MeetupAgenda v-if="meetup['agenda'].length !== 0" :agenda="meetup['agenda']"/>
           <ui-alert v-else>Программа пока пуста...</ui-alert>
         </div>
         <div class="meetup__aside">
